547f893bbdb151fa6f2e629cf0eeca9bf8e0aac0
Vsevolod Bochkarev <bochkarevvv777@mail.ru>
2025-02-08 19:51:30 +0300

    Первый коммит

--- WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -28,23 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.showTextButton = new System.Windows.Forms.Button();
             this.resultLabel = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // showTextButton
-            // 
-            this.showTextButton.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.showTextButton.Font = new System.Drawing.Font("Impact", 25.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.showTextButton.ForeColor = System.Drawing.SystemColors.Control;
-            this.showTextButton.Location = new System.Drawing.Point(523, 248);
-            this.showTextButton.Name = "showTextButton";
-            this.showTextButton.Size = new System.Drawing.Size(211, 61);
-            this.showTextButton.TabIndex = 0;
-            this.showTextButton.Text = "КУПИТЬ!\r\n\r\n";
-            this.showTextButton.UseVisualStyleBackColor = false;
-            this.showTextButton.Click += new System.EventHandler(this.showTextButton_Click);
             // 
             // resultLabel
             // 
@@ -54,7 +40,7 @@
             this.resultLabel.ForeColor = System.Drawing.SystemColors.Control;
             this.resultLabel.Location = new System.Drawing.Point(558, 302);
             this.resultLabel.Name = "resultLabel";
-            this.resultLabel.Size = new System.Drawing.Size(0, 54);
+            this.resultLabel.Size = new System.Drawing.Size(0, 53);
             this.resultLabel.TabIndex = 1;
             this.resultLabel.Click += new System.EventHandler(this.resultLabel_Click);
             // 
@@ -66,7 +52,7 @@
             this.label1.ForeColor = System.Drawing.SystemColors.Control;
             this.label1.Location = new System.Drawing.Point(554, 276);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 42);
+            this.label1.Size = new System.Drawing.Size(0, 41);
             this.label1.TabIndex = 1;
             this.label1.Click += new System.EventHandler(this.resultLabel_Click);
             // 
@@ -79,7 +65,6 @@
             this.ClientSize = new System.Drawing.Size(1166, 387);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.resultLabel);
-            this.Controls.Add(this.showTextButton);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.MaximizeBox = false;
             this.Name = "Form1";
@@ -90,8 +75,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button showTextButton;
         private System.Windows.Forms.Label resultLabel;
         private System.Windows.Forms.Label label1;
     }
